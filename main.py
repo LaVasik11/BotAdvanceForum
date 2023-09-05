1bc473f7c7bb67d51811ee0c18c58d2198ff5951
LaVasik11 <kankiyag@yandex.ru>
2023-09-05 13:17:25 +0400

    Изменил функцию put_reaction, теперь она требует ссылку на ПОСТ а не на страницу, скорость увеличилась.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import create_bot
+import re
+
 
 name_bot = 'CoolBot_V1_№'
 password = 'password2098715'
@@ -11,36 +13,14 @@ quantity_bot = 10
 
 def put_reaction():
 
-    url_to_the_page = input('Введите ссылку на страницу: ')
-    post_number = int(input('Введите номер поста: '))
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    url_to_the_page = input('Введите ссылку на пост: ')
     reaction = input('Выберете реакцию[Клёво/Люблю/Ха-ха/Ничоси/Печалька/Отстой]: ')
 
 
     for i in range(1, quantity_bot+1):
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        driver = webdriver.Chrome(options=options)
-        driver.get(url_to_the_page)
-
-        driver.find_element(By.CLASS_NAME, 'p-navgroup-linkText').click()
-        time.sleep(2)
-        driver.find_elements(By.CLASS_NAME, 'input')[-2].send_keys(f'{name_bot}{i}')
-        driver.find_elements(By.CLASS_NAME, 'input')[-1].send_keys(password)
-        driver.find_elements(By.CLASS_NAME, 'iconic-label')[-1].click()
-        driver.find_elements(By.CLASS_NAME, 'button-text')[-2].click()
-
-        reaction_elements = driver.find_elements(By.CLASS_NAME, 'reaction-text.js-reactionText')
-        target_element = reaction_elements[post_number-1]
-        driver.execute_script("arguments[0].scrollIntoView();", target_element)
-
-
-
-        x = driver.find_elements(By.CLASS_NAME, 'message-inner')[post_number-1]\
-            .find_element(By.CSS_SELECTOR, '.message-cell.message-cell--main')\
-            .find_element(By.CSS_SELECTOR, '.message-main.js-quickEditTarget')\
-            .find_element(By.CSS_SELECTOR, '.message-attribution.message-attribution--split')\
-            .find_element(By.CSS_SELECTOR, '.message-attribution-opposite.message-attribution-opposite--list ')\
-            .find_element(By.TAG_NAME, 'li').find_element(By.TAG_NAME, 'a').get_attribute('href').split('-')[-1]
+        x = url_to_the_page.split('-')[-1]
 
         d = {'Клёво': f'https://forum.advance-rp.ru/posts/{x}/react?reaction_id=1',
              'Люблю': f'https://forum.advance-rp.ru/posts/{x}/react?reaction_id=2',
@@ -57,7 +37,6 @@ def put_reaction():
         confirm_reaction.find_elements(By.CLASS_NAME, 'button-text')[2].click()
         confirm_reaction.find_element(By.CSS_SELECTOR, '.button--primary.button.button--icon.button--icon--confirm').click()
 
-        driver.quit()
         confirm_reaction.quit()
 
 
@@ -85,12 +64,20 @@ def remove_reactions():
 
         driver.quit()
 
+    print('\n', '-'*16, '\n| Реакции убраны |\n', '-'*16)
 
-if __name__ == '__main__':
+
+def main():
+    func_dict = {'Создать ботов': create_bot.create_bot,
+                 'Поставить реакции на пост': put_reaction,
+                 'Убрать реакции': remove_reactions}
+
     print('Доступные варианты: [Создать ботов | Поставить реакции на пост | Убрать реакции]')
     choice = input('Выберите действие: ')
 
-    d = {'Создать ботов': create_bot.create_bot,
-         'Поставить реакции на пост': put_reaction,
-         'Убрать реакции': remove_reactions}
-    d[choice]()
+    func_dict[choice]()
+
+
+if __name__ == '__main__':
+    main()
+
